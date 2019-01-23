b6747e6c119fc96b89365b6d869b17f785ea65cb
SergNif <n777@bk.ru>
2019-01-23 22:27:36 +0300

    make it better

--- web.js
+++ web.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+let server = require('http').Server(app);
+var port = process.env.PORT || 8000
 app.use(express.static(path.join(process.cwd(), 'source')));
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 console.log(process.env.PORT || 5000);
 app.listen(process.env.PORT || 5000);
